2a605febe46cf19c46de20b7c99dc88c9e1ba160
opsb <oliver@opsb.co.uk>
2018-01-11 20:23:40 +0100

    Add rank/file indicators

--- assets/src/Main.elm
+++ assets/src/Main.elm
@@ -1,10 +1,10 @@
 module Main exposing (..)
 
-import Html exposing (Html, text, div, h1, img)
+import Html exposing (Html, div, h1, img)
 import Html.Attributes exposing (src)
 import List.Extra as List
-import Svg exposing (svg, rect, Svg, g)
-import Svg.Attributes exposing (width, height, rx, ry, viewBox, x, y, fill)
+import Svg exposing (svg, rect, Svg, g, text_, text)
+import Svg.Attributes exposing (width, height, rx, ry, viewBox, x, y, fill, fontSize)
 import Arithmetic exposing (isEven)
 
 
@@ -84,7 +84,7 @@ view : Model -> Html Msg
 view model =
     svg
         [ width (toString boardSize), height (toString boardSize), viewBox boardViewBox ]
-        (List.indexedMap rankView model.board)
+        (List.indexedMap rankView (Debug.log "board" model.board))
 
 
 boardViewBox =
@@ -100,10 +100,21 @@ rankView rankIndex rank =
 
 squareView : Int -> Int -> Square -> Svg Msg
 squareView rankIndex fileIndex square =
-    rect
+    svg
         [ x (toString <| rankIndex * squareSize)
-        , y (toString <| fileIndex * squareSize)
-        , width (toString squareSize)
+        , y (toString <| (7 - fileIndex) * squareSize)
+        , width <| toString <| squareSize
+        , height <| toString <| squareSize
+        ]
+        [ squareFillView rankIndex fileIndex square
+        , coordinateAnnotationView rankIndex fileIndex
+        ]
+
+
+squareFillView : Int -> Int -> Square -> Svg Msg
+squareFillView rankIndex fileIndex square =
+    rect
+        [ width (toString squareSize)
         , height (toString squareSize)
         , fill <| squareColor rankIndex fileIndex
         ]
@@ -118,12 +129,57 @@ squareColor rankIndex fileIndex =
         "#d2bba0"
 
 
+coordinateAnnotationView : Int -> Int -> Svg Msg
+coordinateAnnotationView rankIndex fileIndex =
+    g [] <|
+        List.filterMap identity <|
+            [ if fileIndex == 0 then
+                Just <| letterView rankIndex
+              else
+                Nothing
+            , if rankIndex == 0 then
+                Just <| numberView fileIndex
+              else
+                Nothing
+            ]
+
+
+letterView : Int -> Svg Msg
+letterView rankIndex =
+    text_
+        [ fontSize <| toString <| coordsFontSize
+        , x <| toString <| (squareSize - coordsFontSize)
+        , y <| toString <| (8 + squareSize - coordsFontSize)
+        ]
+        [ text (indexToRank rankIndex) ]
+
+
+coordsFontSize =
+    14
+
+
+numberView : Int -> Svg Msg
+numberView fileIndex =
+    text_
+        [ fontSize <| toString <| coordsFontSize
+        , x "5"
+        , y "18"
+        ]
+        [ text <| toString <| fileIndex + 1 ]
+
+
 boardSize =
     600
 
 
 squareSize =
     boardSize // 8
+
+
+indexToRank index =
+    [ "a", "b", "c", "d", "e", "f", "g", "h" ]
+        |> List.getAt index
+        |> Maybe.withDefault ""
 
 
 
